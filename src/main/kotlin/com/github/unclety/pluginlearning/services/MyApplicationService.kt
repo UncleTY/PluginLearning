0de32e6513ba584a2e38e2cee6f62923507e8456
GitHub Action <action@github.com>
2022-03-29 05:56:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/unclety/pluginlearning/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/unclety/pluginlearning/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.unclety.pluginlearning.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.unclety.pluginlearning.MyBundle
 
 class MyApplicationService {
 
